00d130b8a8ea3a130d08db19d38d1e3cdf9840be
Blanc-Poil <tamiottesuchet@gmail.com>
2023-12-04 13:19:29 +0100

    day4

---
+++ day4/day4-2.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define BUFSIZE 1024
+
+#define ARRSIZE 64
+#define HSTSIZE 256
+
+unsigned scan_unsigned(char** cur);
+
+int main(int argc, char* argv[])
+{
+    //no file exception
+    if (argc < 2) {
+        printf("[NO FILE GIVEN !]\n");
+        return 0;
+    }
+
+    // INIT
+    FILE* file = fopen(argv[1], "r");
+    char buffer[BUFSIZE];
+    char* cur = NULL;
+    unsigned sum = 0;
+    // context
+    unsigned winnings[ARRSIZE];
+    unsigned* win = NULL;
+    unsigned* win_end = NULL;
+    unsigned havings[ARRSIZE];
+    unsigned* have = NULL;
+    unsigned* have_end = NULL;
+    unsigned id;
+    unsigned history[HSTSIZE];
+    unsigned* hist = NULL;
+
+    // init history
+    for (size_t i=0 ; i < HSTSIZE ; i++)
+        history[i] = 1;
+
+    // PROCESS
+    while (!feof(file)) {
+        // GET ROW
+        fgets(buffer, BUFSIZE, file);
+        if (!*buffer) break;
+        cur = buffer;
+        // READING
+        // get id
+        while (*cur < '0' || *cur > '9') cur++;
+        id = scan_unsigned(&cur) - 1;
+        do cur++; while (*cur == ' ');
+        hist = history + id + 1;
+        // get winnings
+        win = winnings;
+        while (*cur != '|') {
+            *win = scan_unsigned(&cur);
+            win++;
+            // jump
+            while (*cur == ' ') cur++;
+        }
+        win_end = win;
+        // jump
+        do cur++; while (*cur == ' ');
+        // get havings
+        have = havings;
+        while (*cur != '\n') {
+            *have = scan_unsigned(&cur);
+            have++;
+            // jump
+            while (*cur == ' ') cur++;
+        }
+        have_end = have;
+        // PROCESSING
+        unsigned points = 0;
+        win = winnings;
+        while (win != win_end) {
+            have = havings;
+            while (have != have_end) {
+                if (*have == *win) {
+                    points = (points) ? points*2 : 1;
+                    *hist += history[id];
+                    hist++;
+                    break;
+                }
+                have++;
+            }
+            win++;
+        }
+        sum += history[id];
+        // RESET BUFFER
+        *buffer = '\0';
+    }
+
+    // RESULT
+    printf("Sum: %u\n", sum);
+
+    // return
+    return 0;
+}
+
+unsigned scan_unsigned(char** cur)
+{
+    unsigned value = 0;
+    while (**cur >= '0' && **cur <= '9') {
+        value *= 10;
+        value += **cur - '0';
+        (*cur)++;
+    }
+    return value;
+}
