4df25d1c735f7b51c4ab43f0704fe44f38d0195b
Blanc-Poil <tamiottesuchet@gmail.com>
2023-12-06 09:32:13 +0100

    day6-1~

--- day6/day6-1.c
+++ day6/day6-1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define BUFSIZE 64
 
@@ -23,9 +24,10 @@ int main(int argc, char* argv[])
 
     // INIT
     FILE* file = fopen(argv[1], "r");
-    char buffer[BUFSIZE];
-    char* time_cur = NULL;
-    char* dist_cur = NULL;
+    char buffer_time[BUFSIZE];
+    char buffer_dist[BUFSIZE];
+    char* time_cur = buffer_time;
+    char* dist_cur = buffer_dist;
     unsigned long prod = 1;
     // context
     struct race_data races[ARRSIZE];
@@ -33,7 +35,35 @@ int main(int argc, char* argv[])
     struct race_data* race_end;
 
     // READING
-    
+    fgets(buffer_time, BUFSIZE, file);
+    fgets(buffer_dist, BUFSIZE, file);
+    // jump
+    while (*time_cur != ' ') time_cur++;
+    while (*dist_cur != ' ') dist_cur++;
+    // read loop
+    race = races;
+    while (*time_cur != '\n') {
+        // jump
+        while (*time_cur == ' ') time_cur++;
+        while (*dist_cur == ' ') dist_cur++;
+        race->time = scan_unsigned(&time_cur);
+        race->dist = scan_unsigned(&dist_cur);
+        race++;
+    }
+    race_end = race;
+
+    // PROCESS
+    race = races;
+    while (race != race_end) {
+        unsigned delta = race->time*race->time - 4*race->dist;
+        double sqrt_delta = sqrt(delta);
+        double x1 = (-race->time - sqrt_delta) / (-2);
+        double x2 = (-race->time + sqrt_delta) / (-2);
+        int min = ceil(x1);
+        int max = ceil(x2);
+        min = (min < 0) ? 0 : min;
+        max = (max < 0) ? 0 : max;
+    }
 
     // RESULT
     printf("Prod: %u\n", prod);
