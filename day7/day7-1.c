96c1e777bbc22358233890934e68cfca92a57148
Blanc-Poil <tamiottesuchet@gmail.com>
2023-12-07 10:08:35 +0100

    day7~

---
+++ day7/day7-1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define BUFSIZE 256
+
+unsigned scan_unsigned(char** cur);
+
+int main(int argc, char* argv[])
+{
+    //no file exception
+    if (argc < 2) {
+        printf("[NO FILE GIVEN !]\n");
+        return 0;
+    }
+
+    // INIT
+    FILE* file = fopen(argv[1], "r");
+    char buffer[BUFSIZE];
+    char* cur = NULL;
+    unsigned sum = 0;
+    // context
+
+    // PROCESS
+    while (!feof(file)) {
+        // GET ROW
+        fgets(buffer, BUFSIZE, file);
+        if (!*buffer) break;
+        cur = buffer;
+        // READING
+        // PROCESSING
+        // RESET BUFFER
+        *buffer = '\0';
+    }
+
+    // RESULT
+    printf("Sum: %u\n", sum);
+
+    // return
+    return 0;
+}
+
+unsigned scan_unsigned(char** cur)
+{
+    unsigned value = 0;
+    while (**cur >= '0' && **cur <= '9') {
+        value *= 10;
+        value += **cur - '0';
+        (*cur)++;
+    }
+    return value;
+}
